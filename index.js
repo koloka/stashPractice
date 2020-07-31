d55db51d0588bdad79091623947722293b718c03
SAMBATH <sambath.srey@sparkdigital.asia>
2020-07-31 13:18:40 +0700

    ad

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const number = 1;
-const text = "hello";
+const texts = "hello";
 const color = "blue";
 const cost = 12;
 const range = "0-9";
 const distance = 45;
 const greeting = "hello my name is ";
+
+
+console.log(texts);
+
+
